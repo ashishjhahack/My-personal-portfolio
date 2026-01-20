e52fb88d68907ef6c638263ce27134e353dff091
Ashish Jha <ashishjhag143@gmail.com>
2026-01-20 11:11:21 +0530

    creating footer section

--- src/sections/Home.jsx
+++ src/sections/Home.jsx
@@ -17,9 +17,9 @@ const glowVariants = {    // it is for social icons glow effect
   hover: {
     scale: 1.2, y:-3,
     filter: "drop-shadow(0 0 8px rgba(13, 88, 204, 0.9)) drop-shadow(0 0 18px rgba(16, 185, 129, 0.8))", 
-    transition: {type: 'spring', stiffness: 300, damping: 15}
+    transition: {type: 'spring', stiffness: 300, damping: 15}      // it is used when we hover on the icon
   },
-  tap: {scale: 0.9, y:0, transition: {duration: 0.08}}
+  tap: {scale: 0.9, y:0, transition: {duration: 0.08}}      // it is used when we click on the icon
 }
 
 const Home = () => {
